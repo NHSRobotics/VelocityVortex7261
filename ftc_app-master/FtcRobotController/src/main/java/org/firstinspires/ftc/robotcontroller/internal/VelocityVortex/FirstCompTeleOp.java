029f9ed898eda4a287a2ed134c56bff84f2de992
North High Robotics <scnstem@gmail.com>
2016-10-27 08:01:19 -0500

    Added camera opmode class

--- ftc_app-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/VelocityVortex/FirstCompTeleOp.java
+++ ftc_app-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/VelocityVortex/FirstCompTeleOp.java
@@ -1,18 +1,25 @@
 package org.firstinspires.ftc.robotcontroller.internal.VelocityVortex;
 
+import android.graphics.Bitmap;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.robotcontroller.internal.FirstResq.Movement;
 
+import for_camera_opmodes.OpModeCamera;
+
 /**
  * Created by 7260 on 10/19/2016.
  */
-public class FirstCompTeleOp extends OpMode{
+public class FirstCompTeleOp extends OpModeCamera{
 
     Movement chassis;
+    int ds2 = 2;
 
     public void init() {
         chassis = new Movement(hardwareMap.dcMotor.get("left"), hardwareMap.dcMotor.get("right"));
+        setCameraDownsampling(8);
+        super.init();
     }
 
     public void loop() {
@@ -26,6 +33,13 @@ public class FirstCompTeleOp extends OpMode{
             chassis.leftSpin(.3*Math.pow(gamepad1.right_stick_x, 2));
         } else {
             chassis.stopDrive();
+        }
+
+        if(imageReady()){
+            Bitmap rgbImage;
+            rgbImage = convertYuvImageToRgb(yuvImage, width, height, ds2);
+            telemetry.addData("Width: ", rgbImage.getWidth());
+            telemetry.addData("Height: ", rgbImage.getHeight());
         }
     }
 }
